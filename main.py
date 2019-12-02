6eec4c7bc957f8041f3d3aa1cc26fdedc6eb76ed
Michael Pavlov <mr.michael.pavlov@gmail.com>
2019-12-02 07:38:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,8 +74,22 @@ def get_global_data():
 def serve_json(obj):
     return json.dumps(obj)
 
+# the decorator
+def enable_cors(fn):
+    def _enable_cors(*args, **kwargs):
+        # set CORS headers
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+        if bottle.request.method != 'OPTIONS':
+            # actual request; reply with the actual response
+            return fn(*args, **kwargs)
+
+    return _enable_cors
 
 @bottle.get('/get-attacks')
+@enable_cors
 def get_clusters():
     data, epoch = get_global_data()
 
@@ -87,6 +101,7 @@ def get_clusters():
 
 
 @bottle.get('/get-attack/<key:int>')
+@enable_cors
 def get_cluster(key):
     data, epoch = get_global_data()
 
@@ -105,7 +120,6 @@ def get_cluster(key):
 
 
 def main():
-    response.headers['Access-Control-Allow-Origin'] = '*'
     bottle.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 if __name__ == '__main__':
